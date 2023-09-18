e940aa5f46a473e3f9401ec0c2611ccc9f0167df
Jorge Covello <jorgejfcovello@gmail.com>
2023-09-18 20:06:55 -0300

    fix detach example warnings

--- threads/detach/main.c
+++ threads/detach/main.c
@@ -40,22 +40,30 @@ void restar (int id){
 void crear_hilos () {
     for (int i = 0; i < cantidad_sumadores; i++) {
         pthread_t * sumador = malloc(sizeof(pthread_t));
-        pthread_create(sumador, NULL, sumar, i+1);
+        int* id = malloc(sizeof(int));
+        *id = i+1;
+        pthread_create(sumador, NULL, (void*)sumar, id);
         pthread_detach(*sumador);
     }
     for (int i = 0; i < cantidad_duplicadores; i++) {
         pthread_t * multiplicador = malloc(sizeof(pthread_t));
-        pthread_create(multiplicador, NULL, multiplicar, i+1);
+        int* id = malloc(sizeof(int));
+        *id = i+1;
+        pthread_create(multiplicador, NULL, (void*)multiplicar, id);
         pthread_detach(*multiplicador);
     }
     for (int i = 0; i < cantidad_duplicadores; i++) {
         pthread_t * divisor = malloc(sizeof(pthread_t));
-        pthread_create(divisor, NULL, dividir, i+1);
+        int* id = malloc(sizeof(int));
+        *id = i+1;
+        pthread_create(divisor, NULL, (void*)dividir, id);
         pthread_detach(*divisor);
     }
     for (int i = 0; i < cantidad_duplicadores; i++) {
         pthread_t * resta = malloc(sizeof(pthread_t));
-        pthread_create(resta, NULL, restar, i+1);
+        int* id = malloc(sizeof(int));
+        *id = i+1;
+        pthread_create(resta, NULL, (void*)restar, id);
         pthread_detach(*resta);
     }
 }
